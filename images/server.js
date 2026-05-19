e5325cc9b1fbe1d585ef5410a9cd455215d54179
HARISH <sonkarharish52584@gmail.com>
2026-05-19 14:20:25 +0530

    mongodb connected

--- images/server.js
+++ images/server.js
@@ -7,13 +7,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Working 🚀");
+// MongoDB Connection
+mongoose.connect("mongodb+srv://sonkarharish52584_db_user:Harish1919@cluster0.e2sbv4f.mongodb.net/poojaDB?retryWrites=true&w=majority")
+.then(() => {
+    console.log("MongoDB Connected ✅");
+})
+.catch((err) => {
+    console.log(err);
 });
 
-// routes
+// Test Route
+app.get("/", (req, res) => {
+    res.send("API Working 🚀");
+});
+
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
 });
